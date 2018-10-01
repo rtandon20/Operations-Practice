e5b50d4cc28eeffda0ce68e1dc1e49899ed51423
rtandon20 <43002713+rtandon20@users.noreply.github.com>
2018-10-01 08:32:34 -0400

    Add files via upload

---
+++ src/OperationsPractice.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ * @author Riya Tandon
+ * Operations Practice
+ * 
+ *
+ */
+public class OperationsPractice
+{
+
+	public static void main(String[] args) 
+	{
+		int number;
+		number = 10;
+		System.out.println("Number: " + number);
+		number = number + 5;	// or number += 5;
+		System.out.println("Add 5: " + number);
+		number /= 2;
+		System.out.println("Divide by 2: " + number);
+		number *= 4;
+		System.out.println("Multiply by 4: " + number);
+		number -= 6;
+		System.out.println("Subtract 6: " + number);
+		number %= 2;
+		System.out.println("Modularly divide by 2: " + number);
+
+	}
+
+}
